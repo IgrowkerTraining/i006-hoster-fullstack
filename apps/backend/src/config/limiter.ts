a3b9b44cea8aa4e62fd3ba58d8834571bae52020
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2026-02-18 20:44:24 -0300

    Revert "Creacion de controller y router para Moneda"

--- apps/backend/src/config/limiter.ts
+++
@@ -1,7 +0,0 @@
-import {rateLimit} from 'express-rate-limit'
-
-export const limiter = rateLimit({
-    windowMs: 60 * 1000,
-    limit: 3,
-    message: {"error": "Has alcanzado el límite de peticiones"}
-})
